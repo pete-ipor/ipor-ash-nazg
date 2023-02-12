cf207ca44a1a63d91edc7ead198ab77f04d3d240
Piotr Rzonsowski <pete@ipor.io>
2023-02-12 21:16:00 +0100

    code format

--- src/Session.sol
+++ src/Session.sol
@@ -3,25 +3,27 @@ pragma solidity 0.8.17;
 contract Session {
 
     address public sessionUser;
+
     struct Call {
         address target;
         bytes callData;
     }
+
     function startSession(Call[] calldata calls) public returns (uint256 blockNumber, bytes[] memory returnData) {
         sessionUser = msg.sender;
         blockNumber = block.number;
         uint256 callsNumber = calls.length;
         returnData = new bytes[](callsNumber);
-            bytes32 senderAddressBytes =  senderToBytes(msg.sender);
-        for(uint256 i; i < callsNumber; ++i) {
-            bytes32 addressFromRequest = bytes32(calls[i].callData[4:36]);
+        bytes32 senderAddressBytes = senderToBytes(msg.sender);
+        for (uint256 i; i < callsNumber; ++i) {
+            bytes32 addressFromRequest = bytes32(calls[i].callData[4 : 36]);
             require(addressFromRequest == bytes32(0x00), "should be zero address");
             uint256 length = calls[i].callData.length;
             bytes memory newCallData = new bytes(length);
-            for(uint256 j; j < length; ++j) {
+            for (uint256 j; j < length; ++j) {
                 newCallData[j] = calls[i].callData[j];
-                if(j >= 16 && j < 36) {
-                    newCallData[j] = senderAddressBytes[j-16];
+                if (j >= 16 && j < 36) {
+                    newCallData[j] = senderAddressBytes[j - 16];
                 }
             }
             (bool success, bytes memory ret) = calls[i].target.call(newCallData);
@@ -31,7 +33,7 @@ contract Session {
         }
     }
 
-   function senderToBytes(address sender) public pure returns (bytes32) {
+    function senderToBytes(address sender) public pure returns (bytes32) {
         return bytes32(abi.encodePacked(sender));
     }
 }
